581c7a8f7bfa97de694a7e999bad09d3b71105e9
Pixel Brush <letsplaytvirtmann@gmail.com>
2025-07-20 19:31:32 +0200

    removed binaries

--- user/build.sh
+++ user/build.sh
@@ -2,23 +2,23 @@ as --32 -march=i486 sys.s -o sys
 as --32 -march=i486 crt.s -o crt
 
 gcc -m32 -static -march=i486 --data-sections -fomit-frame-pointer -fno-pic -fno-stack-protector -nostdlib -c shell.c
-ld -m elf_i386 -static -o shell shell.o sys --entry=main -z noexecstack
+ld -m elf_i386 -static -o shell crt sys shell.o --entry=_start -z noexecstack
 strip shell
 
 gcc -m32 -static -march=i486 --data-sections -fomit-frame-pointer -fno-pic -fno-stack-protector -nostdlib -c pitch.c
-ld -m elf_i386 -static -o pitch pitch.o sys --entry=main -z noexecstack
+ld -m elf_i386 -static -o pitch crt sys pitch.o --entry=_start -z noexecstack
 strip pitch
 
 gcc -m32 -static -march=i486 --data-sections -fomit-frame-pointer -fno-pic -fno-stack-protector -nostdlib -c prep.c
-ld -m elf_i386 -static -o prep prep.o sys --entry=main -z noexecstack
+ld -m elf_i386 -static -o prep crt sys prep.o --entry=_start -z noexecstack
 strip prep
 
 gcc -m32 -static -march=i486 --data-sections -fomit-frame-pointer -fno-pic -fno-stack-protector -nostdlib -c ls.c
-ld -m elf_i386 -static -o ls ls.o sys crt --entry=_start -z noexecstack
+ld -m elf_i386 -static -o ls crt sys ls.o --entry=_start -z noexecstack
 strip ls
 
 gcc -m32 -static -march=i486 --data-sections -fomit-frame-pointer -fno-pic -fno-stack-protector -nostdlib -c test.c
-ld -m elf_i386 -static -o test test.o sys --entry=main -z noexecstack
+ld -m elf_i386 -static -o test crt sys test.o --entry=_start -z noexecstack
 strip test
 
 as --32 -march=i486 reboot.s -o reboot
